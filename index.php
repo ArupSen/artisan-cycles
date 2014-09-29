8e26566a459c18a572862c5622fd75858c6a42e5
Arup Sen <arupsen48@gmail.com>
2014-09-29 22:22:41 +0100

    added the image and text

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ require_once INCPATH.'header.php';
 navbar();
 
 // the unique contents will go here up to the footer
+img_tag('home-main.jpg','Artisan Cycles Limited');
 ?>
-
+<p>the finest Italian craftsmanship - in the English countryside</p>
 
 <?php footer(); ?>
