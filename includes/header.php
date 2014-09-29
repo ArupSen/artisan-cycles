7906b1711378deb97956af4d09e0012228cfb91f
Arup Sen <arupsen48@gmail.com>
2014-09-29 22:16:32 +0100

    wrapped headings in header tag, included Oswald bold font

--- includes/header.php
+++ includes/header.php
@@ -23,7 +23,7 @@ include_once DATAPATH.'meta_tags.php';
 // Google web fonts Lato, Lobster and Oswald regular
 // Fontawesome and Bootstrap via bootstrapcdn which should leverage browser caching
 ?>
-<link href='http://fonts.googleapis.com/css?family=Lato|Lobster|Oswald' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Lato|Lobster|Oswald:400,700' rel='stylesheet' type='text/css'>
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="<?php echo CSSPATH.'style.css'; ?>" type="text/css" media="screen" />
@@ -34,6 +34,8 @@ include_once DATAPATH.'meta_tags.php';
         <!--[if lt IE 8]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-<div id="wrap">
-<h1>artisan cycles</h1>
-<h2>hand crafted /// made to measure /// custom built</h2>
+<div id="wrap" class="container">
+<header>
+  <h1>artisan cycles</h1>
+  <h2>hand crafted /// made to measure /// custom built</h2>
+</header>
