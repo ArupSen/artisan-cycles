0c3e1ed5ac26827067da2a6443c3c6ee1121ffde
Arup Sen <arupsen48@gmail.com>
2014-09-29 22:20:14 +0100

    initial commit

---
+++ includes/enquire.php
@@ -0,0 +1,5 @@
+<?php
+// enquire button snippet
+// appears on about, wheels, fitting, frames
+?>
+<p class="enquire"><a href="mailto:info@artisancycles.co.uk" title="enquire">&gt;&gt;&gt; enquire</a></p>
