9062eed89b0aa9bec3a8a4f5ebeea8034eb1298e
Arup Sen <arupsen48@gmail.com>
2014-09-29 13:26:23 +0100

    created a list of pages

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,9 +2,14 @@
 // -- navbar.php --
 ?>
 <ul id="navbar">
-	<li id="nav-home"><a href="index.php" title="home">home</a></li>
-	<li id="nav-about"><a href="about.php" title="about">about</a></li>
-	<li id="nav-contact"><a href="contact.php" title="contact">contact</a></li>
+  <li id="nav-home"><a href="index.php" title="home">home</a></li>
+  <li id="nav-about"><a href="about.php" title="about">about us</a></li>
+  <li id="nav-bikes"><a href="bikes.php" title="our bikes">our bikes</a></li>
+  <li id="nav-frames"><a href="frames.php" title="our frames">our frames</a></li>
+  <li id="nav-fitting"><a href="fitting.php" title="bike fitting">bike fitting</a></li>
+  <li id="nav-wheels"><a href="wheels.php" title="wheel building">wheel building</a></li>
+  <li id="nav-clothing"><a href="clothing.php" title="clothing &amp; shoes">clothing &amp; shoes</a></li>
+  <li id="nav-visits"><a href="visits.php" title="visits">visits</a></li>
 </ul>
 <div id="clear"></div>
 <div id="contents">
