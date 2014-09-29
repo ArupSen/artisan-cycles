7bc620e8ebb948522c74d6297c713a5989a1b811
Arup Sen <arupsen48@gmail.com>
2014-09-29 14:04:59 +0100

    initial commit

---
+++ fitting.php
@@ -0,0 +1,12 @@
+<?php
+// --fitting.php--
+require_once 'inc_path.php';
+require_once INCPATH.'header.php';
+navbar();
+
+// the unique contents will go here up to the footer
+?>
+
+
+
+<?php footer(); ?>
