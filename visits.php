395a4a6f07476687b9872716ae5fe814d7af028a
Arup Sen <arupsen48@gmail.com>
2014-09-30 18:24:21 +0100

    added images and text

--- visits.php
+++ visits.php
@@ -6,7 +6,28 @@ navbar();
 
 // the unique contents will go here up to the footer
 ?>
+<div class="col-md-2 picture-left">
+  <?php
+  img_tag('visits-local.jpg','places to visit in Much Hadham');
+  ?>
+<p><a href="http://twitter.com/henrymoorefdn" target="_blank">@henrymoorefdn</a></p>
+<p><a href="http://twitter.com/hoepley_cafe" target="_blank">@Hoepley_Cafe</a></p>
+</div>
+<div class="col-md-5">
+  <address>The Old Grain Store Bromley Lane
+Much Hadham Hertfordshire SG10 6HU Tel 01438 940600
+Mob 07772 931543 info@artisancycles.co.uk</address>
+<div class="social-icons">
+</div>
+<?php
+img_tag('visits-map.jpg','Local area map of Artisan Cycles');
+?>
+</div>
 
-
-
+<div class="col-md-5 picture-right">
+  <?php
+  img_tag('visits-main.jpg','Visit Artisan Cycles by appointment');
+  ?>
+<p>visits by appointment</p>
+</div>
 <?php footer(); ?>
