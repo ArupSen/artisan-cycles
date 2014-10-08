6ec154e89627567e73a9fba5492ef35236cd3857
Arup Sen <arupsen48@gmail.com>
2014-10-08 13:11:10 +0100

    added rows rides and epoca

--- bikes.php
+++ bikes.php
@@ -6,7 +6,37 @@ navbar();
 
 // the unique contents will go here up to the footer
 ?>
-
+<div class="row rides">
+  <div class="col-md-5 picture">
+    img_tag('bikes-lisa.jpg', 'Lisa riding Ironman UK on Racer Rosa bicycle');
+<p>IRONMAN UK<br />
+LISA ON RACER ROSA</p>
+  </div>
+  <div class="col-md-3 picture">
+    img_tag('bikes-duncan.jpg', 'Duncan on Dragon Ride on his Racer Rosa bicycle');
+<p>DRAGON RIDE<br />
+DUNCAN<br />
+ON RACER ROSA</p>
+  </div>
+  <div class="col-md-4 picture">
+    img_tag('bikes-riccardo.jpg', 'Riccardo on the Tour of Flanders');
+<p>TOUR OF FLANDERS<br /> RICCARDO <br />ON RACER ROSA</p>
+  </div>
+</div>
+<div class="row epoca">
+  <div class="col-md-5 picture">
+    img_tag('bikes-epoca1.jpg','Epoca carbon fibre bike');
+  </div>
+  <div class="col-md-4 picture">
+  <p>EPOCA<br />
+  R60<br />
+  CARBON FIBRE</p>
+    img_tag('bikes-epoca2.jpg', Epoca carbon fibre bike');
+  </div>
+  <div class="col-md-3 picture">
+    img_tag('bikes-epoca3.jpg', Epoca carbon fibre bike');
+  </div>
+</div>
 
 
 <?php footer(); ?>
