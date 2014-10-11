909fb5570a79cfdd5cdb7605a9e915a069808e4f
Arup Sen <arupsen48@gmail.com>
2014-10-11 16:53:23 +0100

    added twitter urls, skype call script, classes for captions

--- visits.php
+++ visits.php
@@ -10,19 +10,28 @@ navbar();
   <?php
   img_tag('visits-local.jpg','places to visit in Much Hadham');
   ?>
-<p><a href="http://twitter.com/henrymoorefdn" target="_blank">@henrymoorefdn</a></p>
-<p><a href="http://twitter.com/hoepley_cafe" target="_blank">@Hoepley_Cafe</a></p>
+<p class="henry-moore"><a href="http://twitter.com/henrymoorefdn" target="_blank">@henrymoorefdn</a></p>
+<p class="cafe"><a href="http://twitter.com/hoepley_cafe" target="_blank">@Hoepley_Cafe</a></p>
 </div>
-<div class="col-md-5">
-  <address>The Old Grain Store Bromley Lane
-Much Hadham Hertfordshire SG10 6HU Tel 01438 940600
-Mob 07772 931543 info@artisancycles.co.uk</address>
+<div class="col-md-5 picture">
+  <address>The Old Grain Store<br /> Bromley Lane<br />
+Much Hadham<br /> Hertfordshire SG10 6HU<br /> Tel 01438 940600<br />
+Mob 07772 931543<br /> info@artisancycles.co.uk</address>
 <div class="social-icons">
-<?php
-img_tag('fb_icon.png', 'Follow us on Facebook');
-img_tag('twitter_icon.png', "Follow us on Twitter");
-img_tag('skype_icon.png','Call us on Skype');
-?>
+<a href="https://www.facebook.com/pages/Artisan-Cycles/280582228781100" title="follow us on facebook" target="_blank"><?php img_tag('fb_icon.png', 'Follow us on Facebook'); ?></a>
+<a href="https://twitter.com/ArtisanCyclesUK" title="follow us on twitter" target="_blank"><?php img_tag('twitter_icon.png', "Follow us on Twitter"); ?></a>
+<script type="text/javascript" src="http://www.skypeassets.com/i/scom/js/skype-uri.js"></script>
+  <div id="SkypeButton_Call_artisancycles_1">
+    <script type="text/javascript">
+      Skype.ui({
+        "name": "chat",
+        "element": "SkypeButton_Call_artisancycles_1",
+        "participants": ["artisancycles"],
+        "imageColor": "white",
+        "imageSize": 32
+      });
+    </script>
+  </div>
 </div>
 <?php
 img_tag('visits-map.jpg','Local area map of Artisan Cycles');
@@ -33,6 +42,6 @@ img_tag('visits-map.jpg','Local area map of Artisan Cycles');
   <?php
   img_tag('visits-main.jpg','Visit Artisan Cycles by appointment');
   ?>
-<p>visits by appointment</p>
+<p class="appointment">visits by appointment</p>
 </div>
 <?php footer(); ?>
