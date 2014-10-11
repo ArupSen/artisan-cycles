c704a728780879114a4b8ea7ff0382536a10e3c3
Arup Sen <arupsen48@gmail.com>
2014-10-11 16:50:33 +0100

    swapped skype default image with custom one

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 // Site specific js code
 (function(){
   'use strict';
+  $('#SkypeButton_Call_artisancycles_1').find('img').attr('src','http://artisancycles.co.uk/new/images/skype_icon.png');
+  $('#SkypeButton_Call_artisancycles_1').find('img').attr('title','call us on Skype');
 }());
