4a7540c1ca11a7158c0781fb70454d4233d747f4
Arup Sen <arupsen48@gmail.com>
2014-10-13 23:25:08 +0100

    added subject line to mailto: link

--- includes/enquire.php
+++ includes/enquire.php
@@ -2,4 +2,4 @@
 // enquire button snippet
 // appears on about, wheels, fitting, frames
 ?>
-<p class="enquire"><a href="mailto:info@artisancycles.co.uk" title="enquire">&gt;&gt;&gt; enquire</a></p>
+<p class="enquire"><a href="mailto:info@artisancycles.co.uk?subject=Enquiry from the website" title="enquire">&gt;&gt;&gt; enquire</a></p>
