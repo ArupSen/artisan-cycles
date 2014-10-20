b8f8c6ed244ca2aedfa64cb75479307d6a5a47b6
Arup Sen <arupsen48@gmail.com>
2014-10-20 20:10:47 +0100

    added index and Index arrays of meta tags

--- data/meta_tags.php
+++ data/meta_tags.php
@@ -7,9 +7,13 @@
 // description - 255 characters
 // keywords - no longer used by google search
 
+$Index = array(
+  'title' => 'hand crafted, made to measure, custom built bikes - Formigli, Legend, Racer Rosa, Tommasini',
+  'description' => 'Artisan Cycles Ltd - hand crafted, made to measure, custom built bikes - Formigli, Legend, Racer Rosa, Tommasini - Made in Italy Parentini cycling clothing and Chrono shoes');
+
 $index = array(
-  'title' => '',
-  'description' => '');
+  'title' => 'hand crafted, made to measure, custom built bikes - Formigli, Legend, Racer Rosa, Tommasini',
+  'description' => 'Artisan Cycles Ltd - hand crafted, made to measure, custom built bikes - Formigli, Legend, Racer Rosa, Tommasini - Made in Italy Parentini cycling clothing and Chrono shoes');
 
 $about = array(
   'title' => '',
