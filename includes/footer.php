2fc928a7ff301bdf3529bcc1ca9bb46fe175235e
Arup Sen <arupsen48@gmail.com>
2014-10-20 20:13:39 +0100

    added HiStats code

--- includes/footer.php
+++ includes/footer.php
@@ -25,5 +25,13 @@
             r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
             ga('create','UA-XXXXX-X');ga('send','pageview');
         </script>
+<!-- Histats.com  START (hidden counter)-->
+<script type="text/javascript">document.write(unescape("%3Cscript src=%27http://s10.histats.com/js15.js%27 type=%27text/javascript%27%3E%3C/script%3E"));</script>
+<a href="http://www.histats.com" target="_blank" title="myspace tracker" ><script  type="text/javascript" >
+try {Histats.start(1,2719976,4,0,0,0,"");
+Histats.track_hits();} catch(err){};
+</script></a>
+<noscript><a href="http://www.histats.com" target="_blank"><img  src="http://sstatic1.histats.com/0.gif?2719976&101" alt="myspace tracker" border="0"></a></noscript>
+<!-- Histats.com  END  -->
 </body>
 </html>
