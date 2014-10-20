3282caf03ec10ae37dfd39f364a1e372c151feeb
Arup Sen <arupsen48@gmail.com>
2014-10-20 20:09:20 +0100

    added small classes for each medium

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 // Site specific js code
 (function(){
   'use strict';
-  $('#SkypeButton_Call_artisancycles_1').find('img').attr('src','http://artisancycles.co.uk/new/images/skype_icon.png');
+  $('#SkypeButton_Call_artisancycles_1').find('img').attr('src','http://artisancycles.co.uk/images/skype_icon.png');
   $('#SkypeButton_Call_artisancycles_1').find('img').attr('title','call us on Skype');
+  // add the small size bootstrap column classes to existing medium
+  $('.col-md-1').addClass('col-sm-1');
+  $('.col-md-2').addClass('col-sm-2');
+  $('.col-md-3').addClass('col-sm-3');
+  $('.col-md-4').addClass('col-sm-4');
+  $('.col-md-5').addClass('col-sm-5');
+  $('.col-md-6').addClass('col-sm-6');
+  $('.col-md-7').addClass('col-sm-7');
+  $('.col-md-8').addClass('col-sm-8');
+  $('.col-md-9').addClass('col-sm-9');
+  $('.col-md-10').addClass('col-sm-10');
+  $('.col-md-11').addClass('col-sm-11');
+  $('.col-md-12').addClass('col-sm-12');
 }());
