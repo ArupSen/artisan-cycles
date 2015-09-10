7acb0a32b77288f82c16ecbb48cac56d6911840c
Arup Sen <arupsen48@gmail.com>
2015-09-10 23:38:22 +0100

    implement mobile optimisations
    -burger toggle menu for small screens
    -strapline moves under main heading
    -resizing for main menu and 992 and 768 breakpoints

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,15 +1,31 @@
 <?php
 // -- navbar.php --
 ?>
-<ul id="navbar">
-  <li id="nav-home"><a href="index.php" title="home">home</a></li>
-  <li id="nav-about"><a href="about.php" title="about">about us</a></li>
-  <li id="nav-bikes"><a href="bikes.php" title="our bikes">our bikes</a></li>
-  <li id="nav-frames"><a href="frames.php" title="our frames">our frames</a></li>
-  <li id="nav-fitting"><a href="fitting.php" title="bike fitting">bike fitting</a></li>
-  <li id="nav-wheels"><a href="wheels.php" title="wheel building">wheel building</a></li>
-  <li id="nav-clothing"><a href="clothing.php" title="clothing &amp; shoes">clothing &amp; shoes</a></li>
-  <li id="nav-visits"><a href="visits.php" title="visits">visits</a></li>
-</ul>
+<nav id="navbar" class="navbar-default" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-nav">
+                <span class="sr-only">Toggle</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+
+        <!-- Links to the toggled -->
+        <div class="collapse navbar-collapse" id="main-nav">
+            <ul id="ul-main-navbar" class="nav navbar-nav">
+                <li id="nav-home"><a href="index.php" title="home">home</a></li>
+                <li id="nav-about"><a href="about.php" title="about">about us</a></li>
+                <li id="nav-bikes"><a href="bikes.php" title="our bikes">our bikes</a></li>
+                <li id="nav-frames"><a href="frames.php" title="our frames">our frames</a></li>
+                <li id="nav-fitting"><a href="fitting.php" title="bike fitting">bike fitting</a></li>
+                <li id="nav-wheels"><a href="wheels.php" title="wheel building">wheel building</a></li>
+                <li id="nav-clothing"><a href="clothing.php" title="clothing &amp; shoes">clothing &amp; shoes</a></li>
+                <li id="nav-visits"><a href="visits.php" title="visits">visits</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
 <div id="clear"></div>
 <div id="contents">
